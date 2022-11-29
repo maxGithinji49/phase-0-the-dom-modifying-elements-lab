b6eb921ca5c6bd5b4ecf23e74ff5fca1093e0a34
Max <maxwell.githinji@student.moringaschool.com>
2022-11-29 22:34:23 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here!
+// describe("index.html", () => {
+//     describe("after index.js is processed", () => {
+//       it("no longer has DOM node 'main#main'", () => {
+//         expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+//       });
+
+const main= document.getElementById ('main');
+main.remove ();
+  
+//       it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//         expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+//       });
+
+const newHeader= document.createElement ('h1');
+newHeader.setAttribute ('id', 'victory');
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+  
+//       it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//         expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+//       });
+
+
+
+  
+//       it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+//         expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+//       });
+  
+//     });
+//   })  
